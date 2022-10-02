c8c26cfe472293f0590db5278a8b8d203056e9dc
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 01:01:42 -0400

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(checkSame);
 
+    function checkSame(value) {
+        return !args.includes(value);
+    }
 };
 
 // Do not edit below this line
