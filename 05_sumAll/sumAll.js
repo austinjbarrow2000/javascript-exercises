0f1cf98ef9b737edf80bff52ea56a8b186969991
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 01:13:45 -0400

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    result = 0;
+    if( num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') { 
+        return 'ERROR';
+    }
 
+    if(num1 < num2) {
+        small = num1;
+        big = num2;
+    } else {
+        small = num2;
+        big = num1;
+    }
+
+    for( let i = small; i <= big; i++ ) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
