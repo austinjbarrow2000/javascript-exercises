32910bd67745812cdb950bdde8a6379ce59be1cc
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 23:57:33 -0400

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if( num < 0 ) { return "OOPS";}
+
+    let num1 = 0;
+    let num2 = 1;
+    for( let i = 1; i < num; i++) {
+        newNum = num1 + num2;
+        num1 = num2;
+        num2 = newNum;
+    }
+    return num2;
 
 };
 
