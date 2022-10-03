dccd5f75c94ec3699512a977a34af18b354f5546
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 23:49:23 -0400

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    a = a.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").toLowerCase().replace(/\s/g,"");
+    b = a.split("");
+    b = b.reverse();
+    b = b.join("");
 
+    return a == b;
 };
 
 // Do not edit below this line
