1e9eade410d06c9b9d3bfad5e60de23857f157f1
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 23:32:44 -0400

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	const totalYears = a.reduce((total, num) => {
+    return total + num;
+  }, 0 )
+
+  return totalYears;
 };
 
-const multiply = function() {
+const multiply = function(a, b) {
+  const result = a.reduce((total, num) => {
+    return total * num;
+  }, 1 )
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  result = 1;
+	for( let i = 1; i <= a; i++ ) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
